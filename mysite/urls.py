935af90492f3661996aa9ff1687888253b793a9d
marcelo chaves <marceloluiz.icg@gmail.com>
2025-05-05 21:15:36 -0400

    Modifica urls para exibir a home na raiz e em /home/

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include  # Use 'include' para incluir as URLs do a
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('app_ebac.urls')),
+    path('', include('app_ebac.urls')),
 ]
